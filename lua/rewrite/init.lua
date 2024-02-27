b7cc9f6aa1876083b95ca9d7a150d037226dba31
Sntz91 <tobias.ziegler.8@gmail.com>
2024-02-27 21:06:36 +0100

    change

--- lua/rewrite/init.lua
+++ lua/rewrite/init.lua
@@ -1,15 +1,12 @@
 local M = {}
 
-print("loaded")
-
-
- function M.go()
-	print(".")
+local function test()
+	print("test")
 end
 
 function M.setup()
 	vim.keymap.set("n", "<leader>l", function()
-		print("test")
+		test()
 	end)
 	print('setup')
 end
